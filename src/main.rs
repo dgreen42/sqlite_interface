be8219e191b04034336044fbc20cfbcbf1d45977
laz <lazgreen32@gmail.com>
2024-09-02 13:05:28 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,81 @@
 // they are not included when you install sqlite3 cause they are C headers? anyways...
 //// apt install libsqlite3-0 libsqlite3-dev
 
-struct Personel {
-    id: i32,
-    name: String,
-    position: String,
+mod company {
+    use colored::Colorize;
+    use std::io::stdin;
+
+    pub struct Shipments {
+        pub id: i32,
+        pub shipment_id: i32,
+        pub contents: String,
+        pub quantity: String,
+    }
+
+    pub struct Personel {
+        pub id: i32,
+        pub name: String,
+        pub position: String,
+    }
+
+    pub fn get_shipment_info() -> Shipments {
+        println!("{}", "Enter Personel ID".cyan());
+        let mut id = String::new();
+        _ = stdin().read_line(&mut id).unwrap().to_string().trim();
+        println!("{}: {:?}", "User ID entered: ".green(), &id);
+
+        println!("{}", "Enter Shipment ID".cyan());
+        let mut shipment_id = String::new();
+        _ = stdin()
+            .read_line(&mut shipment_id)
+            .unwrap()
+            .to_string()
+            .trim();
+        println!("{}: {:?}", "Shipment ID entered: ".green(), &shipment_id);
+
+        println!("{}", "Enter Contents".cyan());
+        let mut contents = String::new();
+        _ = stdin().read_line(&mut contents).unwrap().to_string().trim();
+        println!("{}: {:?}", "Contents: ".green(), &contents);
+
+        println!("{}", "Enter Quantity".cyan());
+        let mut quantity = String::new();
+        _ = stdin().read_line(&mut quantity).unwrap().to_string().trim();
+        println!("{}: {:?}", "Quantity: ".green(), &quantity);
+
+        Shipments {
+            id: id.trim().parse::<i32>().unwrap(),
+            shipment_id: shipment_id.trim().parse::<i32>().unwrap(),
+            contents: contents,
+            quantity: quantity,
+        }
+    }
+
+    pub fn get_personel_info() -> Personel {
+        println!("{}", "Enter Personel ID".cyan());
+        let mut id = String::new();
+        _ = stdin().read_line(&mut id).unwrap().to_string().trim();
+        println!("{}: {:?}", "Personel ID entered: ".green(), &id);
+
+        println!("{}", "Enter Name".cyan());
+        let mut name = String::new();
+        _ = stdin().read_line(&mut name).unwrap().to_string().trim();
+        println!("{}: {:?}", "Name: ".green(), &name);
+
+        println!("{}", "Enter Position".cyan());
+        let mut position = String::new();
+        _ = stdin().read_line(&mut position).unwrap().to_string().trim();
+        println!("{}: {:?}", "Position: ".green(), &position);
+
+        Personel {
+            id: id.trim().parse::<i32>().unwrap(),
+            name: name,
+            position: position,
+        }
+    }
 }
 
-struct Shipments {
-    id: i32,
-    shipment_id: i32,
-}
-
-struct Contents {
-    shipment_id: i32,
-    conents: String,
-    quantity: String,
-}
-
+use company::*;
 use rusqlite::{self, Connection};
 use std::env::args;
 
@@ -31,23 +89,15 @@ fn main() {
         std::process::exit(3);
     }
 
-    let id = args().nth(2).expect("Enter Persons ID");
-
     if option == "-add_shipment" {
-        let shipment_id = args().nth(3).expect("Enter the Shipment ID");
-        let contents = args().nth(4).expect("Enter Shipment Contents");
-        let quantity = args().nth(5).expect("Enter quantity of shipment");
-
-        let current_shipment = Shipments {
-            id: id.trim().parse::<i32>().unwrap(),
-            shipment_id: shipment_id.trim().parse::<i32>().unwrap(),
-        };
-
-        let current_contents = Contents {
-            shipment_id: shipment_id.trim().parse::<i32>().unwrap(),
-            conents: contents,
-            quantity: quantity,
-        };
+        let current_shipment = get_shipment_info();
+        println!(
+            "{},{},{},{}",
+            current_shipment.id,
+            current_shipment.shipment_id,
+            current_shipment.contents,
+            current_shipment.quantity
+        );
 
         let path = "./company.sqlite3";
         let db = Connection::open(path).expect("Could not open database");
@@ -65,38 +115,25 @@ fn main() {
             .unwrap();
 
         let contents_entry = db
-            .execute(
-                &format!(
-                    "INSERT INTO contents (shipment_id, contents, quantity) VALUES ({}, '{}', '{}')",
-                    &current_contents.shipment_id,
-                    &current_contents.conents,
-                    &current_contents.quantity
-                ),
-                (),
-            )
-            .unwrap();
+                    .execute(
+                        &format!(
+                            "INSERT INTO contents (shipment_id, contents, quantity) VALUES ({}, '{}', '{}')",
+                            &current_shipment.shipment_id,
+                            &current_shipment.contents,
+                            &current_shipment.quantity
+                        ),
+                        (),
+                    )
+                    .unwrap();
 
         println!("{:?}", shipments_entry);
         println!("{:?}", contents_entry);
     }
     if option == "-add_user" {
-        let name = args().nth(3).expect("Enter Name of New Personel");
-        let position = args().nth(4).expect("Enter Current Position");
+        let current_person = get_personel_info();
 
         let path = "./company.sqlite3";
         let db = Connection::open(path).expect("Could not open database");
-
-        let current_person = Personel {
-            id: id.trim().parse::<i32>().unwrap(),
-            name: name,
-            position: position,
-        };
-
-        let tester = &format!(
-            "INSERT INTO personel (id, name, position) VALUES ({}, {}, {})",
-            &current_person.id, &current_person.name, &current_person.position,
-        );
-        println!("{:?}", tester);
 
         let personel_entry = db
             .execute(
